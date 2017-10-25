54fb60add419bff649502974ccfd86a25455d982
Will Ashley <william.ashley@fundbox.com>
2017-10-25 11:59:38 +0300

    boilerplate

---
+++ frontend/components/stats.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class Stats extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillMount() {
+
+    }
+
+    render() {
+        return (
+            <div>see me!!</div>
+        )
+    }
+}
