3d4ac2bd8cfda525164343257251f98cdaefbfb8
Will Ashley <william.ashley@fundbox.com>
2017-10-25 12:29:57 +0300

    Add two main components: stats and feed

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import ReactDom from 'react-dom';
-import Player from './components/player';
+import Stats from './components/stats';
+import Feed from './components/feed';
 import './assets/stylesheets/style.less';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const root = document.getElementById('root');
-    ReactDom.render(<Player />, root);
+    const statsRoot = document.getElementById('stats-root');
+    const feedRoot = document.getElementById('feed-root');
+    ReactDom.render(<Stats />, statsRoot);
+    ReactDom.render(<Feed />, feedRoot);
 });
---
+++ frontend/components/feed.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class Feed extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        // Set up web socket
+    }
+
+    render() {
+        return (
+            <div>Feed go here!</div>
+        )
+    }
+}
